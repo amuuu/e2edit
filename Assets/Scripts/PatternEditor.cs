cd6c0148b54e2a81de16084f3caaa82c432a2be4
Keijiro Takahashi <keijiro@gmail.com>
2025-05-26 21:02:04 +0900

    pattern selector

--- Assets/Scripts/PatternEditor.cs
+++ Assets/Scripts/PatternEditor.cs
@@ -28,15 +28,40 @@ public sealed class PatternEditor : MonoBehaviour
         {
             var button = root.Q<Button>("part-select-button-" + i);
             var temp = i;
-            button.clicked += () => _pattern.PartSelect = temp;
+            button.clicked += () => SelectPattern(temp);
         }
 
-        for (var i = 0; i < 64; i++)
+        for (var i = 1; i <= 64; i++)
         {
-            var button = root.Q<Button>($"step-select-button-{(i / 16) + 1}-{(i % 16) + 1}");
-            var temp = i + 1;
-            button.clicked += () => _pattern.StepSelect = temp;
+            var temp = i;
+            GetStepButton(i).clicked += () => SelectStep(temp);
         }
+
+        SelectPattern(1);
+        SelectStep(1);
+    }
+
+    Button GetStepButton(int i)
+      => GetComponent<UIDocument>().rootVisualElement.Q<Button>
+           ($"step-select-button-{((i - 1) / 16) + 1}-{((i - 1) % 16) + 1}");
+
+    void SelectPattern(int i)
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        var prev = root.Q<Button>("part-select-button-" + _pattern.PartSelect);
+        var next = root.Q<Button>("part-select-button-" + i);
+        prev.RemoveFromClassList("part-select-button-selected");
+        next.AddToClassList("part-select-button-selected");
+        _pattern.PartSelect = i;
+    }
+
+    void SelectStep(int i)
+    {
+        var prev = GetStepButton(_pattern.StepSelect);
+        var next = GetStepButton(i);
+        prev.RemoveFromClassList("step-select-button-selected");
+        next.AddToClassList("step-select-button-selected");
+        _pattern.StepSelect = i;
     }
 
     async Awaitable RequestReceivePattern()
