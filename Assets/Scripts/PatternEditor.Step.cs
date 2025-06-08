0d26d7dd9f6ee17f6dffc27d96947d353f29b208
Keijiro Takahashi <keijiro@gmail.com>
2025-06-08 22:13:36 +0900

    step functions

--- Assets/Scripts/PatternEditor.Step.cs
+++ Assets/Scripts/PatternEditor.Step.cs
@@ -4,14 +4,16 @@ using UnityEngine.UIElements;
 
 public sealed partial class PatternEditor : MonoBehaviour
 {
+    #region Step select buttons
+
+    // Button references
+    Button[] _stepButtons = new Button[64];
+
     // Style classes
     static readonly string StepButtonLitClass = "step-select-button-selected";
     static readonly string StepButtonDimClass = "step-select-button-on";
 
-    // Step select buttons
-    Button[] _stepButtons = new Button[64];
-
-    // Step select button factory
+    // Button factory method
     Button CreateStepButton(int index)
     {
         var button = new Button();
@@ -20,7 +22,7 @@ public sealed partial class PatternEditor : MonoBehaviour
         return button;
     }
 
-    // Step spacer factory
+    // Spacer factory method
     VisualElement CreateStepSpacer()
     {
         var spacer = new VisualElement();
@@ -28,7 +30,7 @@ public sealed partial class PatternEditor : MonoBehaviour
         return spacer;
     }
 
-    // Step select button callback
+    // Button callback
     void SelectStep(int i)
     {
         var prev = _stepButtons[_pattern.StepSelect - 1];
@@ -45,18 +47,50 @@ public sealed partial class PatternEditor : MonoBehaviour
         _pattern.StepSelect = i + 1;
     }
 
-    // Step page refresh (called on tab change, etc.)
-    void RefreshStepPage()
+    #endregion
+
+    #region Function buttons
+
+    // Copy button
+    void OnStepCopyButtonPressed()
+      => GUIUtility.systemCopyBuffer = JsonUtility.ToJson(_pattern.CurrentStep);
+
+    // Paste button
+    void OnStepPasteButtonPressed()
     {
-        for (var i = 0; i < 64; i++)
+        try
         {
-            var step = _pattern.GetStepRef(_pattern.PartSelect - 1, i);
-            if (step.onOff > 0)
-                _stepButtons[i].AddToClassList(StepButtonDimClass);
-            else
-                _stepButtons[i].RemoveFromClassList(StepButtonDimClass);
+            _pattern.CurrentStep =
+              JsonUtility.FromJson<MessageSpecs.Step>(GUIUtility.systemCopyBuffer);
         }
+        catch { /* Simply ignores incompatible data */ }
     }
+
+    // Button invocation helper
+    void InvokeButton(Button button)
+    {
+        using (var e = new NavigationSubmitEvent() { target = button } )
+            button.SendEvent(e);
+    }
+
+    // Initialization
+    void InitStepFunctions()
+    {
+        // Function buttons
+        var copyButton = _uiRoot.Q<Button>("step-copy-button");
+        var pasteButton = _uiRoot.Q<Button>("step-paste-button");
+
+        copyButton.clicked += OnStepCopyButtonPressed;
+        pasteButton.clicked += OnStepPasteButtonPressed;
+
+        _uiRoot.RegisterCallback<KeyDownEvent>
+          (evt => { if (evt.keyCode == KeyCode.C) InvokeButton(copyButton);
+                    if (evt.keyCode == KeyCode.V) InvokeButton(pasteButton); });
+    }
+
+    #endregion
+
+    #region Page methods
 
     void InitStepPage()
     {
@@ -81,7 +115,22 @@ public sealed partial class PatternEditor : MonoBehaviour
             }
         }
 
-
         SelectStep(0);
+
+        InitStepFunctions();
     }
+
+    void RefreshStepPage()
+    {
+        for (var i = 0; i < 64; i++)
+        {
+            var step = _pattern.GetStepRef(_pattern.PartSelect - 1, i);
+            if (step.onOff > 0)
+                _stepButtons[i].AddToClassList(StepButtonDimClass);
+            else
+                _stepButtons[i].RemoveFromClassList(StepButtonDimClass);
+        }
+    }
+
+    #endregion
 }
